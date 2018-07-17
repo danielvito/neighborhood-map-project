2cb8a96a629d55397d5ddf2976b45f7c4415481e
Daniel de Vito <daniel_vito@yahoo.com.br>
2018-07-17 01:23:48 -0300

    Adding style to the markers

--- js/maps.js
+++ js/maps.js
@@ -4,10 +4,17 @@ var markers = [];
 var largeInfowindow = null;
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
+     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: -23.6098600, lng: -46.6843567 },
-        zoom: 11
+        zoom: 11,
+        mapTypeControl: false
     });
+
+    // Style the markers a bit. This will be our listing marker icon.
+    var defaultIcon = makeMarkerIcon('0091ff');
+    // Create a "highlighted location" marker color for when the user
+    // mouses over the marker.
+    var highlightedIcon = makeMarkerIcon('FFFF24');
 
     // These are the real estate listings that will be shown to the user.
     // Normally we'd have these in a database instead.
@@ -25,6 +32,7 @@ function initMap() {
             position: position,
             title: title,
             animation: google.maps.Animation.DROP,
+            icon: defaultIcon,
             id: i
         });
         // Push the marker to our array of markers.
@@ -32,6 +40,14 @@ function initMap() {
         // Create an onclick event to open an infowindow at each marker.
         marker.addListener('click', function () {
             populateInfoWindow(this, largeInfowindow);
+        });
+        // Two event listeners - one for mouseover, one for mouseout,
+        // to change the colors back and forth.
+        marker.addListener('mouseover', function () {
+            this.setIcon(highlightedIcon);
+        });
+        marker.addListener('mouseout', function () {
+            this.setIcon(defaultIcon);
         });
         bounds.extend(markers[i].position);
         locations[i].marker = marker;
@@ -45,6 +61,9 @@ function initMap() {
 function populateInfoWindow(marker, infowindow) {
     // Check to make sure the infowindow is not already opened on this marker.
     if (infowindow.marker != marker) {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        marker.setAnimation(null);
+        
         // Clear the infowindow content to give the streetview time to load.
         infowindow.setContent('');
         infowindow.marker = marker;
@@ -84,24 +103,38 @@ function populateInfoWindow(marker, infowindow) {
 
         var location = marker.position.lat() + ',' + marker.position.lng();
         var exploreUrl = 'https://api.foursquare.com/v2/venues/explore?client_id=B4ORRQPXOTBBTMBZKQWPBRPAQU2X0N2F0J1VWB1QDXQNBIJB&client_secret=PHS5UGTSRKYZBSODLPYT2LHI1JDGYBKBPQCJISSURKSHWGUT&v=20180323&limit=1&ll=' + location;
-        $.getJSON(exploreUrl).done(function(data) { 
+        $.getJSON(exploreUrl).done(function (data) {
             var id = data.response.groups[0].items[0].venue.id;
             var tipsUrl = 'https://api.foursquare.com/v2/venues/' + id + '/tips?client_id=B4ORRQPXOTBBTMBZKQWPBRPAQU2X0N2F0J1VWB1QDXQNBIJB&client_secret=PHS5UGTSRKYZBSODLPYT2LHI1JDGYBKBPQCJISSURKSHWGUT&v=20180323&limit=1';
-            $.getJSON(tipsUrl).done(function(data) {
-                    var text = data.response.tips.items[0].text;
-                    var img = data.response.tips.items[0].photo.prefix + data.response.tips.items[0].photo.suffix;
-                    var link = data.response.tips.items[0].canonicalUrl;
+            $.getJSON(tipsUrl).done(function (data) {
+                var text = data.response.tips.items[0].text;
+                var img = data.response.tips.items[0].photo.prefix + data.response.tips.items[0].photo.suffix;
+                var link = data.response.tips.items[0].canonicalUrl;
 
-                    var icon = '<a href="' + link + '" target="_blank"><img src="img/foursquare.png" class="fqIcon"></a>';
+                var icon = '<a href="' + link + '" target="_blank"><img src="img/foursquare.png" class="fqIcon"></a>';
 
-                    $('#foursquare').html('<p>' + text + '</p><p>' + icon + '</p>');
-                })
-                .fail(function() {
-                    $('#foursquare').html('<p>Error to retrive foursquare tips.</p>');
-                });
+                $('#foursquare').html('<p>' + text + '</p><p>' + icon + '</p>');
+            })
+            .fail(function () {
+                $('#foursquare').html('<p>Error to retrive foursquare tips.</p>');
+            });
         })
-        .fail(function() {
+        .fail(function () {
             $('#foursquare').html('<p>Error to retrive foursquare venues.</p>');
         });
     }
+}
+
+// This function takes in a COLOR, and then creates a new marker
+// icon of that color. The icon will be 21 px wide by 34 high, have an origin
+// of 0, 0 and be anchored at 10, 34).
+function makeMarkerIcon(markerColor) {
+    var markerImage = new google.maps.MarkerImage(
+        'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
+        '|40|_|%E2%80%A2',
+        new google.maps.Size(21, 34),
+        new google.maps.Point(0, 0),
+        new google.maps.Point(10, 34),
+        new google.maps.Size(21, 34));
+    return markerImage;
 }
